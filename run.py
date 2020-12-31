143cad4d052fc9a3d298a067efa4ebb14c9019f8
rex <feifanhanmc@163.com>
2020-12-31 15:23:46 +0800

    fix template folder err

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 from flask import Flask, Blueprint, url_for, render_template, request, abort, flash, session, redirect
 from web.index.views import mod as indexModule
 from web.wxuser.views import mod as wxUserModule
@@ -7,8 +8,8 @@ from web.dataexport.views import mod as exportModule
 from web.dataimport.views import mod as importModule
 
 
-def create_app():
-    app = Flask(__name__, template_folder='templates')
+def create_app(static_folder='web/static', template_folder='web/templates'):
+    app = Flask(__name__, static_folder=static_folder, template_folder=template_folder)
     app.register_blueprint(indexModule)
     app.register_blueprint(wxUserModule)
     app.register_blueprint(exportModule)
