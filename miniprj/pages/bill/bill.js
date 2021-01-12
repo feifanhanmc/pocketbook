9484997532070a669aec8e867adeeaadf7d6580e
rex <feifanhanmc@163.com>
2021-01-12 14:36:08 +0800

    q

--- miniprj/pages/bill/bill.js
+++ miniprj/pages/bill/bill.js
@@ -2,17 +2,21 @@
 import regeneratorRuntime from '../..//utils/runtime/runtime';
 import { request } from "../../utils/request/request.js";
 import { login } from "../../utils/asyncwx.js";
+var wxCharts = require('../../utils/wxcharts')
 
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: {
-    // flagRefreshTransData
-    
+  data: {   
     // TransData
     transList: [],
+
+    // ReportData
+    days: [],
+    expend: [],
+    income: [],
 
     // ImageData
     imageNoDataBill: "nodata_bill",
@@ -29,11 +33,60 @@ Page({
     wx.setStorageSync('transList', trans)
     wx.setStorageSync('flagRefreshTransData', false)
   },
+  genReport(id, data, name, color, windowWidth){
+    // 生成图表
+    new wxCharts({
+      canvasId: id,
+      type: 'column',
+      categories: this.data.days,
+      dataPointShape: false,
+      legend: false,
+      series: [{
+          name: name,
+          data: data,
+          color: color,
+          format: function (val) {
+            if(val==0){ return '' }
+            return val
+          }
+      }],
+      yAxis: {
+          title: '金额',
+          min: 0,
+          disabled: true,
+          gridColor: '#ffffff',
+      },
+      xAxis:{
+        disableGrid: true
+      },
+      width: windowWidth,
+      height: 100,
+    });
+  },
+  async showReport() {  
+    const {report} = await request({url:"/wxtrans/show_report",data:{},method:"post"});
+    const {days, expend, income} = report;
+    this.setData({
+      days,
+      expend,
+      income,
+    })
+    wx.setStorageSync('billReport', report)
+    // 获取屏幕宽度
+    let windowWidth = 320;
+    try {
+        let res = wx.getSystemInfoSync();
+        windowWidth = res.windowWidth;
+    } catch (e) {
+      console.log(e)
+    }
+    this.genReport('lineCanvasExpend', this.data.expend, '流出', '#ff0000', windowWidth)
+    this.genReport('lineCanvasIncome', this.data.income, '流入', '#008000', windowWidth)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
 
   /**
@@ -49,9 +102,14 @@ Page({
   onShow: function () {
     if(wx.getStorageSync('flagRefreshTransData')){
       this.showTransList()
+      this.showReport()
     }else{
+      const {days, income, expend} = wx.getStorageSync('billReport')
       this.setData({
-        transList: wx.getStorageSync('transList')
+        transList: wx.getStorageSync('transList'),
+        days,
+        expend,
+        income,
       })
     }
   },
